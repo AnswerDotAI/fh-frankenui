0f0adb86e920c9226959dd85c0f5a10f74b133a8
Isaac Flath <Isaac.flath@gmail.com>
2024-08-10 20:07:10 -0400

    Add Text section

--- docs/main.py
+++ docs/main.py
@@ -28,13 +28,20 @@ def get_all_options(nested_enum):
 @app.get("/")
 async def index():
     return Div(
+        # Themes
         get_header("Themes"),
         get_explanation("Franken UI has a variety of themes that can be used.  To get a CDN with the JS and CSS needed use the `Theme` enum.  For example `Theme.blue.headers()`"),
+        # Buttons
         get_header("Button"),
         get_explanation("Button styling apply to both the A and Button tags.  Example: `A(cls=C(Button.base, Button.style.primary))`"),
         Strong('A: '),*[A(cls=C(fui.Button.base, c))(p) for c,p in get_all_options(fui.Button)],
         Br(),
         Strong('Button: '),*[Button(cls=C(fui.Button.base, c))(p) for c,p in get_all_options(fui.Button)],
+        # Text
+        get_header("Text"),
+        get_explanation("Text styling apply toanything with text.  Example: `P(C(Text.style.lead))`"),
+        Div(cls=fui.Column.four)(*[P(cls=C(c))(p) for c,p in get_all_options(fui.Text)]),
+            
     )
 
 serve()
