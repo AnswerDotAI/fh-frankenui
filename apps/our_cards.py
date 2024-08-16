2f32a43befd68feb364a13e8c3009cc4a858656f
Isaac Flath <Isaac.flath@gmail.com>
2024-08-15 23:07:15 -0400

    Almost done with their card repro

--- apps/our_cards.py
+++ apps/our_cards.py
@@ -1,13 +1,14 @@
 from fasthtml.common import *
 from fh_frankenui.components import *
 from fasthtml.components import Uk_select
-
+from fasthtml.svg import Svg, Path
 
 hdrs = (Script(src="https://cdn.tailwindcss.com"),
         Script(src="https://cdn.jsdelivr.net/npm/uikit@3.21.6/dist/js/uikit.min.js"),
         Script(src="https://cdn.jsdelivr.net/npm/uikit@3.21.6/dist/js/uikit-icons.min.js"),
-        Link(rel="stylesheet", href="https://unpkg.com/franken-wc@0.0.6/dist/css/blue.min.css") 
-)
+        Script(type="module", src="https://unpkg.com/franken-wc@0.0.6/dist/js/wc.iife.js"),
+        Link(rel="stylesheet", href="https://unpkg.com/franken-wc@0.0.6/dist/css/blue.min.css") )
+
 app = FastHTML(hdrs=hdrs,default_hdrs=False, routes = (Mount('/public', StaticFiles(directory='public')),))
 
 
@@ -18,8 +19,7 @@ GalleryCard = Card(H3(cls='uk-h3')("Inline Field Validation"),Br(),
                     UkButton('App', cls=UkButtonT.primary),
                     UkButton('Info', cls=UkButtonT.default),
                     UkButton('Code', cls=UkButtonT.default)),
-                footer_cls='uk-background-muted',
-             )
+                footer_cls='uk-background-muted',)
 
 Left1 = Card(
             Div(cls='grid grid-cols-2 gap-6')(
@@ -36,41 +36,42 @@ Left1 = Card(
 box2svg = Svg(role="img", viewBox="0 0 24 24", cls="mb-3 h-6 w-6")(Path(d="M7.076 21.337H2.47a.641.641 0 0 1-.633-.74L4.944.901C5.026.382 5.474 0 5.998 0h7.46c2.57 0 4.578.543 5.69 1.81 1.01 1.15 1.304 2.42 1.012 4.287-.023.143-.047.288-.077.437-.983 5.05-4.349 6.797-8.647 6.797h-2.19c-.524 0-.968.382-1.05.9l-1.12 7.106zm14.146-14.42a3.35 3.35 0 0 0-.607-.541c-.013.076-.026.175-.041.254-.93 4.778-4.005 7.201-9.138 7.201h-2.19a.563.563 0 0 0-.556.479l-1.187 7.527h-.506l-.24 1.516a.56.56 0 0 0 .554.647h3.882c.46 0 .85-.334.922-.788.06-.26.76-4.852.816-5.09a.932.932 0 0 1 .923-.788h.58c3.76 0 6.705-1.528 7.565-5.946.36-1.847.174-3.388-.777-4.471z", fill="currentColor")),
 
 import calendar
-
+month_opts = [Option(selected=True)(calendar.month_name[1])] + [Option(o) for o in calendar.month_name[2:]]
+year_opts = [Option(selected=True)(2024)] + [Option(o) for o in range(2025,2030)]
 Left2 = Card(
     Div(cls='grid grid-cols-3 gap-4')(
         UkButton(Div(cls='flex flex-col items-center justify-center')(Span(uk_icon='credit-card'),"Card"), cls='uk-button-default h-20 w-full border-2 border-primary'),
         UkButton(box2svg,cls='uk-button-default h-20 w-full')("Card"),
         UkButton(Span(uk_icon='apple'),"Apple",cls='uk-button-default h-20 w-full')),
     
+
     Div(cls=' space-y-4')(
     UkInput('Name','', 'name',placeholder='m@example.com'),
     UkInput('Card Number','', 'card_number',placeholder='m@example.com'),
     Div(cls='grid grid-cols-3 gap-4')(
-        UkDropdownButton('Month', calendar.month_name, btn_cls=UkButtonT.default),
-        UkDropdownButton('Year', range(2024,2030), btn_cls=UkButtonT.default),
-        UkInput('','', 'cvv',placeholder='CVV'),
+        UkSelect(*month_opts,label='Expires',id='expire_month'),
+        UkSelect(*year_opts,label='Year',id='expire_year'),
+        UkInput('CVV','', 'cvv',placeholder='CVV'),
     )),
     header=(H3(cls='uk-h3')('Payment Method'),P(cls='text-sm uk-text-muted')('Add a new payment method to your account.')),
 )
-
+area_opts = ('Team','Billing','Account','Deployment','Support')
+severity_opts = ('Severity 1 (Highest)', 'Severity 2', 'Severity 3', 'Severity 4 (Lowest)')
 Right1 = Card(
-    Div(cls="uk-drop uk-dropdown", uk_dropdown=True)(
-            Ul(cls="uk-dropdown-nav uk-nav")(
-                Li(cls="uk-active")(A(href="#")('Active')),
-                Li(A(href="#")('Item')),
-                Li(cls="uk-nav-header")('Header'),
-                Li(A(href="#")('Item')),
-                Li(A(href="#")('Item')),
-                Li(cls="uk-nav-divider"),
-                Li(A(href="#")('Item')))),
+    Div(cls='grid grid-cols-2 gap-2')(
+        UkSelect(*[Option(o) for o in area_opts],label='Area',id='area'),
+        UkSelect(*[Option(o) for o in severity_opts],label='Severity',id='area')),
+    UkInput(label='Subject',placeholder='I need help with'),
+    UkTextArea(label='Description',placeholder='Please include all information relevant to your issue'),
+    UkFormLabel(label="Tags",state="danger", value="Spam,Invalid"),
+    body_cls='space-y-6 py-0',
     header=(H3('Report an issue'),P(cls='text-sm uk-text-muted')('What area are you having problems with')),
     footer = (UkButton(cls=(UkButtonT.ghost))('Cancel'),UkButton(cls=(UkButtonT.primary))('Submit')),
     footer_cls='flex justify-between')
 
 Right2 = Card(H4(cls='uk-h4')("franken/ui"),
-              P(cls='text-sm text-muted-foreground')("HTML-first, framework-agnostic, beautifully designed components that you can truly copy and paste into your site. Accessible. Customizable. Open Source."),
-              Div(cls='flex space-x-4 text-sm text-muted-foreground')(
+              P(cls='text-sm uk-text-muted')("HTML-first, framework-agnostic, beautifully designed components that you can truly copy and paste into your site. Accessible. Customizable. Open Source."),
+              Div(cls='flex space-x-4 text-sm uk-text-muted')(
               Div(cls='flex items-center')("TypeScript"),
               Div(cls='flex items-center')(Span(uk_icon='star'),"20k"),
               "Updated April 2023"
@@ -78,13 +79,13 @@ Right2 = Card(H4(cls='uk-h4')("franken/ui"),
               )
 
 Right3 = Card(
-    UkInput(label = Div(H4(cls='uk-h5')('Strictly Necessary'),P(cls='font-normal leading-snug text-muted-foreground')('These cookies are essential in order to use the website and use its features.')),
+    UkInput(label = Div(H4(cls='uk-h5')('Strictly Necessary'),P(cls='font-normal leading-snug uk-text-muted')('These cookies are essential in order to use the website and use its features.')),
                 inp_cls='uk-toggle-switch uk-toggle-switch-primary flex-none',
                 cls='flex items-center justify-between gap-2'),
-    UkInput(label = Div(H4(cls='uk-h5')('Functional Cookies'),P(cls='font-normal leading-snug text-muted-foreground')('These cookies allow the website to provide personalized functionality.')),
+    UkInput(label = Div(H4(cls='uk-h5')('Functional Cookies'),P(cls='font-normal leading-snug uk-text-muted')('These cookies allow the website to provide personalized functionality.')),
                 inp_cls='uk-toggle-switch uk-toggle-switch-primary flex-none',
                 cls='flex items-center justify-between gap-2'),
-    UkInput(label = Div(H4(cls='uk-h5')('Performance Cookies'),P(cls='font-normal leading-snug text-muted-foreground')('These cookies help to improve the performance of the website.')),
+    UkInput(label = Div(H4(cls='uk-h5')('Performance Cookies'),P(cls='font-normal leading-snug uk-text-muted')('These cookies help to improve the performance of the website.')),
                 inp_cls='uk-toggle-switch uk-toggle-switch-primary flex-none',
                 cls='flex items-center justify-between gap-2'),
 
@@ -96,10 +97,10 @@ Right3 = Card(
 
 team_members = [{"name": "Sofia Davis", "email": "m@example.com", "role": "Owner"},{"name": "Jackson Lee", "email": "p@example.com", "role": "Member"},]
 
-options = ((Div('Viewer'),Div(cls='text-sm text-muted-foreground')('Can view and comment.',)),
-        (Div('Developer'),Div(cls='text-sm text-muted-foreground')('Can view, comment and edit.',)),
-        (Div('Billing'),Div(cls='text-sm text-muted-foreground')('Can view, comment and manage billing.',)),
-        (Div('Owner'),Div(cls='text-sm text-muted-foreground')('Admin-level to all resources.')),)
+options = ((Div('Viewer'),Div(cls='text-sm uk-text-muted')('Can view and comment.',)),
+        (Div('Developer'),Div(cls='text-sm uk-text-muted')('Can view, comment and edit.',)),
+        (Div('Billing'),Div(cls='text-sm uk-text-muted')('Can view, comment and manage billing.',)),
+        (Div('Owner'),Div(cls='text-sm uk-text-muted')('Admin-level to all resources.')),)
 
 body = [Div(cls='flex items-center space-x-4')(
         Span(cls='relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full bg-accent')(
@@ -107,7 +108,7 @@ body = [Div(cls='flex items-center space-x-4')(
         ),
         Div(cls='flex-1')(
             P(member['name'], cls='text-sm font-medium leading-none'),
-            P(member['email'], cls='text-sm text-muted-foreground')
+            P(member['email'], cls='text-sm uk-text-muted')
         ),
         UkDropdownButton(member['role'], options, btn_cls=UkButtonT.default),
     ) for member in team_members]
@@ -115,7 +116,7 @@ body = [Div(cls='flex items-center space-x-4')(
 
 Middle1 = (Card(*body,
     body_cls='space-y-6',
-    header = (H4('Team Members', cls='uk-h4'),Div('Invite your team members to collaborate.', cls='mt-1.5 text-sm text-muted-foreground'))
+    header = (H4('Team Members', cls='uk-h4'),Div('Invite your team members to collaborate.', cls='mt-1.5 text-sm uk-text-muted'))
 ))
 
 team_members = [
@@ -124,12 +125,7 @@ team_members = [
     {"name": "Sofia Davis", "email": "p@example.com", "role": "Viewer"},
 ]
 
-options = (
-    (Div('Viewer'), Div(cls='text-sm text-muted-foreground')('Can view and comment.')),
-    (Div('Developer'), Div(cls='text-sm text-muted-foreground')('Can view, comment and edit.')),
-    (Div('Billing'), Div(cls='text-sm text-muted-foreground')('Can view, comment and manage billing.')),
-    (Div('Owner'), Div(cls='text-sm text-muted-foreground')('Admin-level to all resources.')),
-)
+options = ("Read and write access", "Read-only access")
 
 body = [Div(cls='flex items-center space-x-4')(
     Span(cls='relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full bg-accent')(
@@ -137,9 +133,10 @@ body = [Div(cls='flex items-center space-x-4')(
     ),
     Div(cls='flex-1')(
         P(member['name'], cls='text-sm font-medium leading-none'),
-        P(member['email'], cls='text-sm text-muted-foreground')
+        P(member['email'], cls='text-sm uk-text-muted')
     ),
-    UkDropdownButton(member['role'], options, btn_cls=UkButtonT.default),
+    UkSelect(*[Option(o) for o in options],label=member['role']),
+    # UkDropdownButton(member['role'], options, btn_cls=UkButtonT.default),
 ) for member in team_members]
 
 Middle2 = Card(
@@ -150,36 +147,27 @@ Middle2 = Card(
     H4(cls='text-sm font-medium')('People with access'),
     *body,
     body_cls='space-y-6',
-    header = (H4('Share this document', cls='uk-h4'),Div('Anyone with the link can view this document.', cls='mt-1.5 text-sm text-muted-foreground'))
+    header = (H4('Share this document', cls='uk-h4'),Div('Anyone with the link can view this document.', cls='mt-1.5 text-sm uk-text-muted'))
 )
 
-Middle3 = Card()
+Middle3 = Card(UkButton(cls=UkButtonT.default)('Jan 20, 2024 - Feb 09, 2024'))
 Middle4 = Card(
-        header = (H4('Notification', cls='uk-h4'),Div('Choose what you want to be notified about.', cls='mt-1.5 text-sm text-muted-foreground'))
-
+    Ul(cls="uk-nav uk-nav-secondary")
+    (
+        *[Li(cls='-mx-1')(A(cls="", href='#demo', uk_toggle=True)(Div(cls="flex gap-x-4")(Div(uk_icon=icon),Div(cls='flex-1')(P(name),P(cls="text-sm uk-text-muted")(desc)))))
+            for icon, name, desc in (('bell','Everything',"Email digest, mentions & all activity."),
+                                    ('user',"Available","Only mentions and comments"),
+                                    ('ban',"Ignoring","Turn of all notifications"))]
+        
+        ),
+    #bell user ban
+        header = (H4('Notification', cls='uk-h4'),Div('Choose what you want to be notified about.', cls='mt-1.5 text-sm uk-text-muted')),
+        body_cls='pt-0'
 )
-# Ul(cls='uk-nav uk-nav-secondary')(
-#     Li(cls='-mx-1')(A(href='#demo', uk_toggle='', role='button', cls='')(
-#             Div(cls='flex gap-x-4')(
-#                 Div(cls='flex-1')(P('Everything'),P('Email digest, mentions & all activity.', cls='text-sm text-muted-foreground'))))),
-#     Li(cls='uk-active -mx-1')(
-#         A(href='#demo', uk_toggle='', role='button', cls='')(
-#             Div(cls='flex gap-x-4')(
-#                 Div(cls='flex-1')(
-#                     P('Available'),
-#                     P('Only mentions and comments.', cls='text-sm text-muted-foreground')
-#                 )
-#             )
-#         )
-#     ),
-#     Li(cls='-mx-1')(
-#         A(href='#demo', uk_toggle='', role='button', cls='')(
-#             Div(cls='flex gap-x-4')(Div(cls='flex-1')(P('Ignoring'),P('Turn off all notifications.', cls='text-sm text-muted-foreground')
-#                 )
-#             )
-#         )
-#     )
-# )
+
+
+
+
 
 
 @app.get('/')
@@ -189,11 +177,11 @@ def home():
     
 
     return Div(cls=('uk-child-width-1-3@l','uk-child-width-1-2@m'), uk_grid=True)(
-            Div(cls='space-y-6')(map(Div,(Left1,Left2))),
+            Div(cls='space-y-6')(map(Div,(Left1,Left2,GalleryCard))),
             Div(cls='space-y-6')(map(Div,(Middle1,Middle2,Middle3,Middle4))),
             Div(cls='space-y-6')(map(Div,(Right1,Right2, Right3))),        
 
-            GalleryCard
+
 
             
     )
