9d5ebf828c1e86dc167af6062b4679c05800f212
Isaac Flath <Isaac.flath@gmail.com>
2024-12-16 15:55:02 -0500

    Update llms files

--- tools/update.sh
+++ tools/update.sh
@@ -3,4 +3,4 @@ llms_txt2ctx nbs/llms.txt --optional true >nbs/llms-ctx-full.txt
 llms_txt2ctx nbs/llms.txt >nbs/llms-ctx.txt
 
 # Need to remove Pico CSS stuff from context files
-pysym2md --output_file nbs/apilist.txt fh_frankenui
+pysym2md --output_file nbs/apilist.txt monsterui 
